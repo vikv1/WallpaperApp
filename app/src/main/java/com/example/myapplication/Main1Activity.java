3f9da8d25c427f9696d922772881d19484a73a2e
Vikrant Verma <87392159+crayzeerr@users.noreply.github.com>
2022-05-12 08:37:01 -0700

    Update Main1Activity.java

--- app/src/main/java/com/example/myapplication/Main1Activity.java
+++ app/src/main/java/com/example/myapplication/Main1Activity.java
@@ -48,7 +48,7 @@ public class Main1Activity extends AppCompatActivity {
 
     }
 
-    public void getImage(View arg0) { //getImage and onActivityResult both get image from user gallery
+    public void getImage(View arg0) { //getImage and onActivityResult both work together to get image from user gallery
         Intent i = new Intent(
                 Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
