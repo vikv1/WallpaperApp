6a64fae6b052c8d758fcc170af22e33085e67f0b
Vikrant Verma <87392159+crayzeerr@users.noreply.github.com>
2022-05-11 17:22:07 -0700

    Added a couple comments.
    # Conflicts:
    #	app/src/main/java/com/example/myapplication/WelcomeScreen.java

--- app/src/main/java/com/example/myapplication/WallpaperDriver.java
+++ app/src/main/java/com/example/myapplication/WallpaperDriver.java
@@ -9,7 +9,7 @@ import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class WelcomeScreen extends AppCompatActivity {
+public class WallpaperDriver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +40,7 @@ public class WelcomeScreen extends AppCompatActivity {
 
     }
     public void changeToMain(View view) {
-        startActivity(new Intent(WelcomeScreen.this, Main1Activity.class));
+        startActivity(new Intent(WallpaperDriver.this, Main1Activity.class));
 
     }
 }
